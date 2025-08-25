ec291955192dd09862f38b6111185b415702c6e4
PriyanshiJain018 <pri.jain018@gmail.com>
2025-08-25 20:34:08 +0530

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,79 +1,21 @@
-// js/router.js
 export class Router {
-  constructor() {
-    this.routes = {};
-    this.currentRoute = null;
-  }
-  
-  addRoute(path, handler) {
-    this.routes[path] = handler;
-  }
-  
-  init() {
-    window.addEventListener('popstate', () => {
-      this.handleRoute(window.location.pathname);
-    });
-    this.handleRoute(window.location.pathname);
-  }
-  
-  navigate(path) {
-    window.history.pushState({}, '', path);
-    this.handleRoute(path);
-  }
-  
-  handleRoute(path) {
-    const { route, params } = this.parsePath(path);
-    
-    if (this.routes[route]) {
-      this.currentRoute = route;
-      this.routes[route](params);
-    } else {
-      for (const [routePattern, handler] of Object.entries(this.routes)) {
-        const match = this.matchRoute(routePattern, path);
-        if (match) {
-          this.currentRoute = routePattern;
-          handler(match);
-          return;
-        }
-      }
-      this.navigate('/');
-    }
-  }
-  
-  parsePath(path) {
-    const parts = path.split('/').filter(Boolean);
-    const params = {};
-    const queryIndex = path.indexOf('?');
-    
-    if (queryIndex > -1) {
-      const queryString = path.substring(queryIndex + 1);
-      const pairs = queryString.split('&');
-      pairs.forEach(pair => {
-        const [key, value] = pair.split('=');
-        params[key] = decodeURIComponent(value);
-      });
+    constructor() {
+        this.routes = {};
+        this.init();
     }
     
-    return { route: path.split('?')[0], params };
-  }
-  
-  matchRoute(pattern, path) {
-    const patternParts = pattern.split('/').filter(Boolean);
-    const pathParts = path.split('/').filter(Boolean);
-    
-    if (patternParts.length !== pathParts.length) return null;
-    
-    const params = {};
-    
-    for (let i = 0; i < patternParts.length; i++) {
-      if (patternParts[i].startsWith(':')) {
-        const paramName = patternParts[i].substring(1);
-        params[paramName] = pathParts[i];
-      } else if (patternParts[i] !== pathParts[i]) {
-        return null;
-      }
+    init() {
+        window.addEventListener('popstate', () => this.handleRoute());
+        this.handleRoute();
     }
     
-    return params;
-  }
+    handleRoute() {
+        const path = window.location.pathname.replace('/JinaSaraswati', '') || '/';
+        console.log('Routing to:', path);
+    }
+    
+    navigate(path) {
+        window.history.pushState({}, '', `/JinaSaraswati${path}`);
+        this.handleRoute();
+    }
 }
